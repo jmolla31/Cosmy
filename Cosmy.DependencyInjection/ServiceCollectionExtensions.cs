adeea9f13efedf4acc259619b5f44c15023992cf
Javi Mollà <javimollamico@gmail.com>
2019-10-03 17:31:35 +0200

    Added dependency injection project

---
+++ Cosmy.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Cosmy.Config;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Cosmy.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCosmyClient(this IServiceCollection services, CosmyConfiguration configuration)
+        {
+            var client = new DocumentClient(new Uri(configuration.Endpoint), configuration.Key);
+
+            services.AddSingleton(configuration);
+            services.AddSingleton(client);
+            services.AddSingleton<ICosmyClient, CosmyClient>();
+            return services;
+        }
+
+        public static IServiceCollection AddCosmyClient(this IServiceCollection services, string endpoint, string key)
+        {
+            var configuration = new CosmyConfiguration
+            {
+                Endpoint = endpoint,
+                Key = key
+            };
+
+            var client = new DocumentClient(new Uri(configuration.Endpoint), configuration.Key);
+
+            services.AddSingleton(configuration);
+            services.AddSingleton(client);
+            services.AddSingleton<ICosmyClient, CosmyClient>();
+            return services;
+        }
+    }
+}
