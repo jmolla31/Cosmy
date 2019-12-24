f2aef61338999923c0feae175ef25f683378b711
Javi Mollà <jmolla31@gmail.com>
2019-12-24 14:58:23 +0100

    Updated interface and nuget metadata

--- Cosmy/ICosmyClient.cs
+++ Cosmy/ICosmyClient.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Cosmy
 {
     public interface ICosmyClient
     {
+        IQueryable<T> CreateDocumentQuery<T>(string collection, object partitionKey = null);
+        Task DeleteDocumentAsync(Guid documentId, string collection, object partitionKey = null);
+        Task DeleteDocumentAsync(string documentId, string collection, object partitionKey = null);
+        Task<IEnumerable<TResult>> ExecuteQuery<T, TResult>(IQueryable<T> source) where T : class;
+        Task<IEnumerable<T>> ExecuteQuery<T>(IQueryable<T> source) where T : class;
+        Task<T> GetDocumentAsync<T>(Guid documentId, string collection, object partitionKey = null) where T : class;
+        Task<T> GetDocumentAsync<T>(string documentId, string collection, object partitionKey = null) where T : class;
+        Task UpdateDocumentAsync<T>(Guid documentId, string collection, T data, object partitionKey = null) where T : class;
+        Task UpdateDocumentAsync<T>(string documentId, string collection, T data, object partitionKey = null) where T : class;
     }
 }
